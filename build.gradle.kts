9e42061820a6d485c125c059fe7b31d5a3f0f34f
machiav3lli <antonios.hazim@tutanota.com>
2024-07-15 00:49:47 +0200

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("com.android.tools.build:gradle:8.5.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
