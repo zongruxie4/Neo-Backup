bfdc728bd60e21834191ed44a8cb3ba4aea0aae5
hg42 <hg42@users.noreply.github.com>
2024-08-14 01:54:32 +0200

    beyondBoundsPageCount = 0

--- src/main/java/com/machiav3lli/backup/ui/navigation/Pager.kt
+++ src/main/java/com/machiav3lli/backup/ui/navigation/Pager.kt
@@ -45,7 +45,18 @@ fun SlidePager(
     pageItems: ImmutableList<NavItem>,
     pagerState: PagerState,
 ) {
-    HorizontalPager(modifier = modifier, state = pagerState, beyondBoundsPageCount = 3) { page ->
+    // it does not make sense, to look for slight optimizations but recompose
+    // all pages on every state change (and other reactivity), so probably all the time
+    // I actually see pauses (busy is off) because of this
+    // breakpoints in backupItems are triggered a lot while none is visible
+    // I tried beyondBoundsPageCount = 0 and I think it is fast enough,
+    // if we would *really* want speed, I would remove animations
+    // (also, users can set time to 0 or use the switch in accessibility settings)
+    // another way would be using another kind of transition animation, e.g.
+    // sliding away the current page but fading in the new page
+    // that would give immediate feedback (slide)
+    // but compose would have more time to render the page when fading it in
+    HorizontalPager(modifier = modifier, state = pagerState, beyondBoundsPageCount = 0) { page ->
         pageItems[page].content()
     }
 }
