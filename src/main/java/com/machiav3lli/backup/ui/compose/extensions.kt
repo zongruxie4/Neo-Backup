8e60752e9a8313a85bf9dd1504345a5970160c6e
hg42 <hg42@users.noreply.github.com>
2024-08-14 02:38:56 +0200

    balanced wrap: remove references to "text" elements, better comments

--- src/main/java/com/machiav3lli/backup/ui/compose/extensions.kt
+++ src/main/java/com/machiav3lli/backup/ui/compose/extensions.kt
@@ -225,12 +225,22 @@ fun LazyListState.isAtBottom() = remember {
 }.value
 
 
-// implement a row layout that balances height (and width)
-// of wrapping elements that are marked with balancedWrap()
-// * marked elements are usually Text (remove other modifiers like weight and width)
+// hg42: this implements a row layout that balances height (and width)
+// of the wrapping elements marked by balancedWrap()
+// * marked elements are usually Text
+// * remove other modifiers that disturb the layout like weight and width
+// * the width of the marked elements is adjusted so that they get a similar height
 // * spare space is distributed evenly between marked elements
-// * the area of marked elements is determined
-// * the width of the marked elements is adjusted so that they get a similar height as far as this is possible in one
+// * for balancing the height
+//   [A] the area of all marked elements is determined (intrinsic should usually be in unwrapped state)
+//   [W] the individual and total width of the marked elements is determined
+//   [H] target height is total area / total width of the marked elements
+//   [w] each marked element is set to width[i] = area[i]/height
+//   [F] each element is fianally measured with the resulting width as constraint
+// problem:
+//   I don't see a way how to find marked sub-elements in the tree
+//   (to say soemthing like: "this element *contains* a marked element")
+
 
 const val BALANCED_WRAP = "BalancedWrap"
 
@@ -249,83 +259,91 @@ fun BalancedWrapRow(
         modifier = modifier,
         measurePolicy = { measurables, constraints ->
 
-            val minBalancedWidth = 10.dp.toPx().toInt()
+            val minBWrapWidth = 10.dp.toPx().toInt()
 
-            val isText = measurables.map {
+            val isBWrap = measurables.map {
                 it to (it.layoutId.toString() == BALANCED_WRAP)
             }.toMap()
-            val nTexts = max(1,  isText.count { it.value })
+            val nBWraps = isBWrap.count { it.value }
 
             // measure with initial constraints
             val maxWidths =
                 measurables.map { it to it.maxIntrinsicWidth(0) }
             val widths = maxWidths.toMap().toMutableMap()
-            // calculate area of elements to be balanced in height
+
+            // [A] calculate area of elements to be balanced in height
             val areas = measurables.map {
-                val width = max(minBalancedWidth, it.maxIntrinsicWidth(0))
+                val width = max(minBWrapWidth, it.maxIntrinsicWidth(0))
                 val height = it.maxIntrinsicHeight(width)
                 it to (width * height)
             }.toMap()
-            val totalBalancedArea = areas.map { (measurable, area) ->
-                if (isText[measurable] == true)
+            val totalBWrapArea = areas.map { (measurable, area) ->
+                if (isBWrap[measurable] == true)
                     area
                 else
                     0
             }.sum()
 
-            // get width of fixed and balanced elements
+            // [W] get total width of fixed and balanced elements
             val maxWidth = constraints.maxWidth
             val totalFixedWidth = maxWidths.map { (measurable, minWidth) ->
-                if (isText[measurable] == true) 0 else minWidth
+                if (isBWrap[measurable] == true) 0 else minWidth
             }.sum()
-            val totalBalancedWidth = maxWidths.map { (measurable, minWidth) ->
-                if (isText[measurable] == true)
-                    max(minBalancedWidth, minWidth)
+            val totalBWrapWidth = maxWidths.map { (measurable, minWidth) ->
+                if (isBWrap[measurable] == true)
+                    max(minBWrapWidth, minWidth)
                 else
                     0
             }.sum()
-            val maxBalancedWidth = maxWidth - totalFixedWidth
-            //val totalWidth = totalFixedWidth + totalTextWidth
-            val balancedHeight = (totalBalancedArea / maxBalancedWidth)
 
-            // determine new widths
-            if (totalBalancedWidth > maxBalancedWidth) {
+            // maximum width of balanced elements within constraints
+            val maxBWrapWidth = maxWidth - totalFixedWidth
+            //val totalWidth = totalFixedWidth + totalBalancedWidth
+
+            // [H] estimate target height
+            // the factor puts the the estimation on the better looking side
+            val balancedHeight = (totalBWrapArea / maxBWrapWidth) * 1.25f
+
+            // [w] determine new widths
+            if (totalBWrapWidth > maxBWrapWidth) {
+                // not enough space, so we need to wrap the balanced elements
                 measurables.map { measurable ->
                     val width = (widths[measurable] ?: 0)
-                    // use a factor because it's estimated
-                    val factor = 1.25f
-                    val adjustedWidth = if (isText[measurable] == true)
-                        (areas[measurable]!! / (balancedHeight * factor)).toInt()
+                    val adjustedWidth = if (isBWrap[measurable] == true)
+                        (areas[measurable]!! / balancedHeight).toInt()   // <--------
                     else
                         width
                     widths[measurable] = adjustedWidth
                 }
-                val finalTotalBalancedWidth = widths.map { (measurable, width) ->
-                    if (isText[measurable] == true)
-                        max(minBalancedWidth, width)
+                val finalTotalBWrapWidth = widths.map { (measurable, width) ->
+                    if (isBWrap[measurable] == true)
+                        max(minBWrapWidth, width)
                     else
                         0
                 }.sum()
-                val addSpace = (maxBalancedWidth - finalTotalBalancedWidth) / nTexts
+                val addSpace = if (nBWraps > 0) (maxBWrapWidth - finalTotalBWrapWidth) / nBWraps else 0
                 measurables.map { measurable ->
                     val width = (widths[measurable] ?: 0)
-                    val adjustedWidth = if (isText[measurable] == true)
+                    val adjustedWidth = if (isBWrap[measurable] == true)
                         max(0, width + addSpace)
                     else
                         width
                     widths[measurable] = adjustedWidth
                 }
             } else {
-                val addSpace = (maxBalancedWidth - totalBalancedWidth) / nTexts
+                // spare space is distributed evenly
+                val addSpace = if (nBWraps > 0) (maxBWrapWidth - totalBWrapWidth) / nBWraps else 0
                 measurables.map { measurable ->
                     val width = (widths[measurable] ?: 0)
-                    val adjustedWidth = if (isText[measurable] == true)
+                    val adjustedWidth = if (isBWrap[measurable] == true)
                         width + addSpace
                     else
                         width
                     widths[measurable] = adjustedWidth
                 }
             }
+
+            // [F] final measure with the new widths as constraints
             val placeables =
                 measurables.map { measurable ->
                     val width = (widths[measurable] ?: 0)
@@ -340,6 +358,7 @@ fun BalancedWrapRow(
             // get final height
             val maxHeight = placeables.map { it.height }.maxOrNull() ?: 0
 
+            // place all elements
             layout(maxWidth, maxHeight) {
                 var xPosition = 0
                 placeables.forEach {
