6d82d243ecd846a1c6218cfabc8478e0dce7d1eb
machiav3lli <antonios.hazim@tutanota.com>
2024-10-27 02:25:50 +0200

    Update: Revamp query handling logic in MainVM

--- src/main/java/com/machiav3lli/backup/pages/MainPage.kt
+++ src/main/java/com/machiav3lli/backup/pages/MainPage.kt
@@ -25,13 +25,13 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -76,11 +76,7 @@ fun MainPage(
         OABX.main?.finishAffinity()
     }
 
-    var query by rememberSaveable {
-        mutableStateOf(
-            viewModel.searchQuery.value ?: ""
-        )
-    }
+    val query by viewModel.searchQuery.collectAsState("")
 
     FullScreenBackground {
         NeoNavigationSuiteScaffold(
