2a63edfad5a8990363e9995cb9b878efd4e0017b
machiav3lli <antonios.hazim@tutanota.com>
2025-02-27 02:19:51 +0100

    Add: Controllable flow extension

---
+++ src/main/java/com/machiav3lli/backup/utils/extensions/Flow.kt
@@ -0,0 +1,19 @@
+package com.machiav3lli.backup.utils.extensions
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
+fun <T> Flow<T>.takeUntilSignal(signal: Flow<Boolean>): Flow<T> = channelFlow {
+    val signalJob = launch {
+        signal.distinctUntilChanged()
+            .collect { if (it) close() } // Close the channel on signal
+    }
+    try {
+        collect { send(it) }
+    } catch (_: Exception) {
+    } finally {
+        signalJob.cancel()
+    }
+}
