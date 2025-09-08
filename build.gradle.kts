e5079e0894a3cd8f2d4196f3ab58caf25d379318
machiav3lli <antonios.hazim@tutanota.com>
2025-09-08 02:25:32 +0200

    Add: Compose ui-tooling-preview to better support compose previews

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,6 +187,7 @@ dependencies {
     implementation(libs.compose.runtime)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.foundation)
     implementation(libs.compose.material3)
     implementation(libs.compose.material3.navigationsuite)
