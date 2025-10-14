f7eaa9d91a0fde24f1a29a5a4c9b0d9557e74926
machiav3lli <antonios.hazim@tutanota.com>
2025-10-14 21:37:58 +0200

    Fix: Running ScheduleReceiver on reboot (closes #983)

---
+++ src/main/java/com/machiav3lli/backup/manager/services/BootReceiver.kt
@@ -0,0 +1,32 @@
+/*
+ * Neo Backup: open-source apps backup and restore app.
+ * Copyright (C) 2025  Antonios Hazim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.machiav3lli.backup.manager.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.machiav3lli.backup.utils.scheduleAlarmsOnce
+import org.koin.core.component.KoinComponent
+
+class BootReceiver : BroadcastReceiver(), KoinComponent {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Thread { scheduleAlarmsOnce(context) }
+        } else return
+    }
+}
